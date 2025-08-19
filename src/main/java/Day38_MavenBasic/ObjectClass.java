77dd616b9fbb6c07247c39e7b84dc46dad264496
itsprinsprays <prncbntz@gmail.com>
2025-08-19 19:48:52 +0800

    Initial commit - Maven project

---
+++ src/main/java/Day38_MavenBasic/ObjectClass.java
@@ -0,0 +1,16 @@
+package Day38_MavenBasic;
+
+public class ObjectClass {
+	
+	public String sampleMethod() {
+		return "Sample Method";
+	}
+	
+	  public static void main(String[] args) {
+		  
+	        ObjectClass obj = new ObjectClass();
+	        System.out.println(obj.sampleMethod());
+	        
+	    }
+
+}
---
+++ src/test/java/Day38_MavenBasic/sampleMethodTest.java
@@ -0,0 +1,16 @@
+package Day38_MavenBasic;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class sampleMethodTest {
+
+	@Test
+	void testSampleMethod() {
+		 ObjectClass obj = new ObjectClass();
+	        assertEquals("Sample Method", obj.sampleMethod());
+	}
+
+}
+	
